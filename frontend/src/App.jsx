5d1ce173f10718c6483b0d1aa98b77de04df4d73
Nguyen H. K. Bui <nguyenbui1996@gmail.com>
2024-12-06 11:19:31 -0800

    git add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginAuthentication from "./pages/LoginAuthentication";
+
+
+const App = () => {
+    return (
+        <div>
+            <LoginAuthentication />
+        </div>
+    );
+};
+
+export default App;
