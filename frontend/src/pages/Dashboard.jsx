94d64f2ef38e3fe1e1f3bb4e127a275f5d12ff37
Nguyen H. K. Bui <nguyenbui1996@gmail.com>
2025-03-26 15:32:21 -0700

    update layout to be responsive

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,17 +5,15 @@ import RightPanel from "../components/Dashboard/03_Right panel/RightPanel.jsx";
 
 const Dashboard = () => {
     return (
-        <main className="w-screen h-screen bg-black">
+        <main className="w-screen min-h-screen bg-black">
             <Header/>
-            <div className="px-[205.5px] pt-[6px] min-h-[882px]">
-                <div className="grid grid-cols-3 gap-1">
-                    <div className="min-w-[428px] bg-ops-gray border border-ops-border"><LeftPanel/></div>
-                    <div className="min-w-[426px] bg-ops-gray border border-ops-border"><CenterPanel/></div>
-                    <div className="min-w-[428px] bg-ops-gray border border-ops-border"><RightPanel/></div>
-                </div>
+            <div className="flex flex-wrap w-full min-h-4xl gap-1 py-1 items-center justify-center">
+                <div className="w-full max-w-md min-h-[870px] max-h-[870px] bg-ops-gray border border-ops-border"><LeftPanel/></div>
+                <div className="w-full max-w-md min-h-[870px] max-h-[870px] bg-ops-gray border border-ops-border"><CenterPanel/></div>
+                <div className="w-full max-w-md min-h-[870px] max-h-[870px] bg-ops-gray border border-ops-border"><RightPanel/></div>
             </div>
         </main>
     )
 }
 
-export default Dashboard
+export default Dashboard;
